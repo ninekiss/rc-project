d15a2761fcef4a2f9f87b64a0a19be02aa8fdc7a
nk <nk-reply@gmail.com>
2023-03-09 14:43:28 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="/">
+          <img src="/logo.svg" className="logo" />
+        </a>
+      </div>
+      <h1>RC PROJECT</h1>
+    </div>
+  )
+}
+
+export default App
